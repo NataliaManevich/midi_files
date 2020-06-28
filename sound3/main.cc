795530588c3973c3481a217d84f0a2f3959a2c38
Natalia <natalia.artworks@gmail.com>
2020-06-28 23:26:02 +0500

    Правильный разбор событий.

--- sound3/main.cc
+++ sound3/main.cc
@@ -12,8 +12,22 @@
 #include "SoundSource.h"
 #include "WaveGenerator.h"
 
-int main(int, char**){
+#pragma pack(push, 1)
+struct  MThdHeader{
+	uint32_t type;
+	uint32_t length;
+	uint16_t format;
+	uint16_t ntrks;
+	uint16_t division;
+};
+struct MTrkHeader{
+	uint32_t type;
+	uint32_t length;
+};
+#pragma pack(pop)
 
+int main(int, char**)
+{
 	static const double s_freq_table[] = {
 			32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00,
 			 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78,
@@ -32,6 +46,7 @@ int main(int, char**){
 			 21096.16, 22350.61, 23679.64, 25087.71, 26579.50, 28160.00, 29834.48, 31608.53,
 			 33488.07, 35479.38, 37589.09, 39824.25, 42192.33, 44701.21, 47359.29, 50175.42
 };
+
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -61,193 +76,171 @@ int main(int, char**){
 	drv.set_sound_source(src);
 	drv.set_master_volume(4000);
 
-	std::vector<std::vector<char>> track;
-
 	std::fstream file;
 	file.open("boomer.mid", std::ios::in | std::ios::binary);
 
-	int chunk_type;
-	int MThd_length;
-	short MThd_format;
-	short MThd_tracks;
-	short MThd_division;
+	MThdHeader mthd;
+	file.read(reinterpret_cast<char*>(&mthd), sizeof(mthd));
 
-	file.read(reinterpret_cast<char*>(&chunk_type), sizeof(chunk_type));
-	if (chunk_type == (
-			'M'*0x00000001 +
-			'T'*0x00000100 +
-			'h'*0x00010000 +
-			'd'*0x01000000))
-		std::cout << "О, это заголовок!" << std::endl;
+	mthd.length = ntohs(mthd.length);
+	mthd.format = ntohs(mthd.format);
+	mthd.ntrks = ntohs(mthd.ntrks);
+	mthd.division = ntohs(mthd.division);
 
-	file.read(reinterpret_cast<char*>(&MThd_length), sizeof(MThd_length));
-	MThd_length = ntohl(MThd_length);
-	std::cout << "Длина MIDI: " << MThd_length << std::endl;
+	std::cout << "MIDI файл, формат: " <<  mthd.format <<
+			", дорожек: " << mthd.ntrks <<
+			", division: " << mthd.division << std::endl;
 
-	file.read(reinterpret_cast<char*>(&MThd_format), sizeof(MThd_format));
-	MThd_format = ntohs(MThd_format);
-	std::cout << "Формат MIDI: " << MThd_format << std::endl;
-
-	file.read(reinterpret_cast<char*>(&MThd_tracks), sizeof(MThd_tracks));
-	MThd_tracks = ntohs(MThd_tracks);
-	std::cout << "Треков MIDI: " << MThd_tracks << std::endl;
-
-	file.read(reinterpret_cast<char*>(&MThd_division), sizeof(MThd_division));
-	MThd_division = ntohs(MThd_division);
-	std::cout << "MIDI division: " << MThd_division << std::endl;
-	if (MThd_division & 0x8000) {
-		std::cout << "Единица измерения - SMPTE" << std::endl;
-	} else {
-		std::cout << "Единица измерения - TPQN" << std::endl;
+	std::vector<std::vector<uint8_t>> track;
+	track.resize(mthd.ntrks);
+	for(unsigned i = 0; i < mthd.ntrks; i++){
+		MTrkHeader mtrk;
+		file.read(reinterpret_cast<char*>(&mtrk), sizeof(mtrk));
+		mtrk.length = ntohl(mtrk.length);
+		std::cout << "Дорожка " << i << " длина: " << mtrk.length << std::endl;
+		track[i].resize(mtrk.length);
+		file.read(reinterpret_cast<char*>(&track[i][0]), mtrk.length);
 	}
 
-	track.resize(MThd_tracks);
+	unsigned trk = 0;
+	unsigned ptr = 0;
 
-	for (int i = 0; i < MThd_tracks; i++) {
-		file.read(reinterpret_cast<char*>(&chunk_type), sizeof(chunk_type));
-		if (chunk_type == (
-				'M'*0x00000001 +
-				'T'*0x00000100 +
-				'r'*0x00010000 +
-				'k'*0x01000000)) {
-			std::cout << "Дорожка: " << i << std::endl;
-
-		} else {
-			std::cout << "<!> Это не дорожка <!>" << std::endl;
-			break;
-		}
-		int trk_length;
-		file.read(reinterpret_cast<char*>(&trk_length), sizeof(int));
-		trk_length = ntohl(trk_length);
-		std::cout << "  Длина: " << trk_length <<std::endl;
-		track[i].resize(trk_length);
-		file.read(&track[i][0], trk_length);
-	}
-
-	size_t trk = 0;
-	size_t p = 0;
-
-	std::cout << "Для демонстрации будем работать с дорожкой: " << trk << std::endl;
+	std::cout << "-> Текущая дорожка: " << trk << std::endl;
 
 	unsigned event_type = 0;
 
-	while (p < track[trk].size()){
+	while (ptr < track[trk].size()){
 		unsigned delta = 0;
-		unsigned delta_v;
+		unsigned delta_part;
 		do{
-			delta_v = track[trk][p++];
-			delta <<= 7; delta+= delta_v & 0x7f;
-		}while (delta_v & 0x80);
+			delta_part = track[trk][ptr++];
+			delta <<= 7; delta+= delta_part & 0x7f;
+		}while (delta_part & 0x80);
+
+		if (delta > 0) SDL_Delay(delta);
 
 		std::cout  << "Дельта: " << delta << std::endl;
 
-		unsigned new_event = track[trk][p++];
+		unsigned new_event = track[trk][ptr++];
 		if (new_event >= 0x80)
 			event_type = new_event;
 		else
-			p--;
+			ptr--;
 
-	if (delta > 0) SDL_Delay(delta);
-		switch(event_type & 0xff) {
-			case 0xff:{
-				unsigned meta_type = track[trk][p++];
-				unsigned meta_len = track[trk][p++];
-				switch (meta_type & 0xff) {
-					case 0x00:
-					{
-						uint16_t seqnum = 0;
-						seqnum += track[trk][p++] * 0x100U;
-						seqnum += track[trk][p++];
-						std::cout << "Sequence Number: " <<
-							std::dec << seqnum << std::hex << std::endl;
-					}
-					break;
+		switch(event_type) {
 
-					case 0x03:
-					{
-						std::string name;
-						while (meta_len --)
-							name += char(track[trk][p++]);
-						std::cout << "Track Name: " << name << std::endl;
-					}
-					break;
+		case 0xff:{
+			unsigned meta_type = track[trk][ptr++];
+			unsigned meta_len = track[trk][ptr++];
+			switch (meta_type) {
+				case 0x00:{
+					uint16_t seqnum = 0;
+					seqnum += track[trk][ptr++] * 0x100U;
+					seqnum += track[trk][ptr++];
+					std::cout << "Sequence number: " <<
+					std::dec << seqnum << std::hex << std::endl;
+				}break;
 
-					case 0x2f:
-						std::cout << "End of Track" << std::endl;
-						return 0;
-					break;
+				case 0x03:{
+					std::string name;
+					while (meta_len --)
+						name += char(track[trk][ptr++]);
+					std::cout << "Track Name: " << name << std::endl;
+				}break;
+
+				case 0x51:{
+					unsigned temp = track[trk][ptr++];
+					for(int i=0;i<2;i++){ temp = (temp << 8) | (track[trk][ptr++]);}
+					std::cout << "Set Tempo: " << temp << " microseconds per MIDI quarter-note" << std::endl;
+				}break;
+
+				case 0x58:{
+					unsigned nn = track[trk][ptr++];
+					unsigned dd = track[trk][ptr++];
+					unsigned num_tick = track[trk][ptr++];
+					unsigned num_part = track[trk][ptr++];
+					std::cout << "Размер " << nn << "/" << dd
+							<< ", колличество тиков в ударе метронома " << num_tick
+							<< ", колличество 32-х долей в " << num_tick << " тиках " << num_part << std::endl;
+
+				}break;
+
+				case 0x2f:
+					std::cout << "End of Track" << std::endl;
+					return 0;
+				break;
+
+				default:
+					std::cout << "неизвестное мета событие " <<
+					std::setw(2) << meta_type << std::endl;
+					return 1;
 				}
 			}break;
 
 			case 0x80 ... 0x8f:{
-				unsigned note = track[trk][p++];
-				unsigned velocity = track[trk][p++];
+				unsigned note = track[trk][ptr++];
+				unsigned velocity = track[trk][ptr++];
 				fg->set_freq(s_freq_table[note]);
 				fg->set_volume(velocity/127.);
+				std::cout << "note OFF: " << note << ", velocity: " << velocity << std::endl;
+				std::cout.flush();
 				fg->key_off();
 			}break;
 
 			case 0x90 ... 0x9f:{
-				unsigned note = track[trk][p++];
-				unsigned velocity = track[trk][p++];
+				unsigned note = track[trk][ptr++];
+				unsigned velocity = track[trk][ptr++];
 				fg->set_freq(s_freq_table[note]);
 				fg->set_volume(velocity/127.);
-				std::cout << "Нота: " << note << ", Скорость: " << velocity << std::endl;
+				std::cout << "note ON: " << note << ", velocity: " << velocity << std::endl;
 				std::cout.flush();
 				fg->key_on();
 			}break;
 
-			case 0xa0 ... 0xaf:
+			case 0xa0 ... 0xaf:{
 
-			break;
+			}break;
+
 			case 0xb0 ... 0xbf:{
 				unsigned control, value;
-				control = track[trk][p++];
-				value = track[trk][p++];
+				control = track[trk][ptr++];
+				value = track[trk][ptr++];
 				std::cout << "Control Change: " <<
 						std::setw(2) << control << " to " <<
 						std::setw(2) << value << std::endl;
 			}break;
+
 			case 0xc0 ... 0xcf:{
 				unsigned program;
-				program = track[trk][p++];
+				program = track[trk][ptr++];
 				std::cout << "Program Change: " <<
 						std::setw(2) << program << std::endl;
 			}break;
+
 			case 0xd0 ... 0xdf:{
 				unsigned aftertouch;
-				aftertouch = track[trk][p++];
+				aftertouch = track[trk][ptr++];
 				std::cout << "Aftertouch: " <<
 						std::setw(2) << aftertouch << std::endl;
 			}break;
+
 			case 0xe0 ... 0xef:{
 				unsigned pitchWheel;
-				pitchWheel = track[trk][p++];
+				pitchWheel = track[trk][ptr++];
 				std::cout << "Pitch Wheel Change: " <<
 						std::setw(2) << pitchWheel << std::endl;
 			}break;
 
 			default:
-				std::cout << " Неизвестное событие: "<< std::hex << std::setfill('0') << std::setw(2) << event_type << std::endl;
-			return 1;
+				std::cout << "неизвестное событие " <<
+				std::setw(2) << event_type << std::endl;
+				return 1;
+
 		}
-	}
 
 	file.close();
 
-	auto win = std::shared_ptr<SDL_Window>(
-			SDL_CreateWindow("", 50 , 50, 800, 600, SDL_WINDOW_SHOWN),
-		SDL_DestroyWindow);
-	SDL_Event ev;
-	for (;;) {
-		while (SDL_PollEvent(&ev)) {
-			if (ev.type == SDL_QUIT) goto the_end;
-		}
-
-		SDL_Delay(10);
 	}
-
-the_end:
 
 	return 0;
 }
